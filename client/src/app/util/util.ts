a14725bb4f9a5bf03f9ea258b52b8f08c4880623
MosCs <mosfox.fep@gmail.com>
2022-10-09 16:16:16 +0700

    Inventory complete

--- client/src/app/util/util.ts
+++ client/src/app/util/util.ts
@@ -1,4 +1,4 @@
-//อ่ํานคุกกี้จําก Browser : https://stackoverflow.com/questions/51109559/get-cookie-with-react
+//อ่ํานคุกกี้จาก Browser : https://stackoverflow.com/questions/51109559/get-cookie-with-react
 export function getCookie(key: string) {
   const b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
   return b ? b.pop() : "";
